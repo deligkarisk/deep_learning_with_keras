d48cb11415ae8bff0d2a84ce8b5fc6d3a7fa2f4b
deligkarisk <deligkarisk@gmail.com>
2022-04-27 13:44:50 +0900

    adding script with data augmentation

---
+++ cats_dogs_classification/cats_dogs_classification_with_image_augment.py
@@ -0,0 +1,85 @@
+from keras import layers
+from keras import models
+from tensorflow import keras
+from tensorflow import optimizers
+from keras.preprocessing.image import ImageDataGenerator
+import os
+import matplotlib.pyplot as plt
+
+
+original_dataset_dir = '/home/kosmas-deligkaris/cats_dogs_kaggle/'
+
+train_dir = os.path.join(original_dataset_dir, 'train')
+validation_dir = os.path.join(original_dataset_dir, 'validation')
+test_dir = os.path.join(original_dataset_dir, 'test')
+
+train_cats_dir = os.path.join(train_dir, 'cats')
+train_dogs_dir = os.path.join(train_dir, 'dogs')
+
+validation_cats_dir = os.path.join(validation_dir, 'cats')
+validation_dogs_dir = os.path.join(validation_dir, 'dogs')
+
+test_cats_dir = os.path.join(test_dir, 'cats')
+test_dogs_dir = os.path.join(test_dir, 'dogs')
+
+
+
+
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D(2, 2))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D(2, 2))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D(2, 2))
+model.add(layers.Flatten())
+model.add(layers.Dropout(0.5))
+model.add(layers.Dense(512, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+model.summary()
+
+model.compile(loss='binary_crossentropy', optimizer=optimizers.RMSprop(lr=1e-4), metrics=['acc'])
+
+train_datagen = ImageDataGenerator(rescale=1./255,
+                                   rotation_range=40,
+                                   width_shift_range=0.2,
+                                   height_shift_range=0.2,
+                                   shear_range=0.2,
+                                   zoom_range=0.2,
+                                   horizontal_flip=True)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(train_dir, target_size=(150, 150), batch_size=20, class_mode='binary')
+validation_generator = test_datagen.flow_from_directory(validation_dir, target_size=(150, 150), batch_size=20, class_mode='binary')
+
+for data_batch, labels_batch in train_generator:
+    print("data batch shape: ", data_batch.shape)
+    print("labels batch shape: ", labels_batch.shape)
+    break
+
+history = model.fit_generator(train_generator, steps_per_epoch=100, epochs=50, validation_data=validation_generator, validation_steps=50)
+model.save('cats_dogs_with_augment_1.h5')
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(acc) + 1)
+
+plt.plot(epochs, acc, 'bo', label="Training acc")
+plt.plot(epochs, val_acc, 'b', label="Validation acc")
+plt.title("Training and validation accuracy")
+plt.legend()
+
+plt.figure()
+
+plt.plot(epochs, loss, 'bo', label="Training loss")
+plt.plot(epochs, val_loss, 'b', label="Validation loss")
+plt.title("Training and validation loss")
+plt.legend()
+
+plt.show()
